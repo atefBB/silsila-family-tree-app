084e94d3e1d8199c920ab36e68f6e4d505626c8a
Atef Ben Ali <atef.bettaib@gmail.com>
2021-10-10 18:57:52 +0100

    build: add `barryvdh/laravel-translation-manager` package to app deps

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Barryvdh\TranslationManager\ManagerServiceProvider::class,
         /*
          * Application Service Providers...
          */
