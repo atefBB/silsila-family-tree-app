2ae85903547adae87165d120087161984736a555
Atef Ben Ali <atef.bettaib@gmail.com>
2021-10-10 14:14:36 +0100

    feat: add `switching to arabic language` link in `layouts partials nav` view file

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -20,7 +20,8 @@
 
             <ul class="nav navbar-nav navbar-right">
                 <?php $mark = (preg_match('/\?/', url()->current())) ? '&' : '?';?>
-                <li><a href="{{ url(url()->current() . $mark . 'lang=en') }}">en</a></li>
+                <li><a href="{{ url(url()->current() . $mark . 'lang=en') }}">English</a></li>
+                <li><a href="{{ url(url()->current() . $mark . 'lang=ar') }}">العربية</a></li>
                 @if (Auth::guest())
                     <li><a href="{{ route('login') }}">Login</a></li>
                     <li><a href="{{ route('register') }}">Register</a></li>
