a949c00d3f589d419bc1c8ffe9f12396ff27b174
Atef Ben Ali <atef.bettaib@gmail.com>
2021-10-10 14:15:10 +0100

    fix: use `laravel build-in translation` in `layouts partials nav` view file

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -23,8 +23,8 @@
                 <li><a href="{{ url(url()->current() . $mark . 'lang=en') }}">English</a></li>
                 <li><a href="{{ url(url()->current() . $mark . 'lang=ar') }}">العربية</a></li>
                 @if (Auth::guest())
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
+                    <li><a href="{{ route('login') }}">{{ __("auth.login") }}</a></li>
+                    <li><a href="{{ route('register') }}">{{ __("auth.register") }}</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
